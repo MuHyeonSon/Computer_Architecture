f4ac9cbf668d498bd4daf6d9c1eb5fe7856f35c5
Sonmoohyeon <smuhyeon84@gmail.com>
2022-06-19 02:57:23 +0900

    Add files via upload

---
+++ Jump.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int PC = 400000;
+int target_address = 0;
+
+int main()
+{
+    //printf("target address : ");
+    //scanf("%d", &target_address); 
+    //PC = target_address;
+    int a = 0;
+    scanf("%d",&a);
+    printf("\n%d",a);
+    
+}
